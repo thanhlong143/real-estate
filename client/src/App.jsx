609a8f64f75c347ec8de97e3ded6190342c5140d
Thanh Long <thanhlongprrt@gmail.com>
2024-09-05 20:32:27 +0700

    start

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button"
+
+const App = () => {
+  return (
+    <div className="h-screen w-screen grid place-content-center">
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
